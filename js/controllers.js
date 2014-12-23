91b0bf6e63e8a01b75ef45817c0eea66285a143d
Nic Yeeles <n.yeeles@gmail.com>
2014-12-23 00:54:24 +0000

    Adds dynamic API urls based on user input - not yet working

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,17 @@
-var myApp = angular.module('myApp',[]);
+angular.module('myApp',[])
+	.controller('MyController', function($scope, $http) {
 
-myApp.controller('MyController', function MyController($scope) {
+		// $http.get('http://gdata.youtube.com/feeds/api/users/smosh?alt=json').success(function(data){
+		// 	$scope.channel = data;
+		// });
 
-	$scope.author = {
-		'name': 'Nic',
-		'age': 25,
-		'gender': 'male'
-	};
-});
+		$scope.channel = {
+			'name': 'smosh',
+			'url_start': 'http://gdata.youtube.com/feeds/api/users/',
+			'url_end':'?alt=json',
+			'api': ''
+		};
+
+		$scope.channel["api"] = $scope.channel.url_start + $scope.channel.name + $scope.channel.url_end;
+
+	});
